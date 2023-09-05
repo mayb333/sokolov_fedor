1cc69c87cc0e0aaed68fe0d79c4fa062f2fcbcb1
Sokolov Fedor <sokol_f4@mail.ru>
2023-09-05 15:10:20 +0300

    first commit

---
+++ kek.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    string symbol;
+
+    cout<<"Hello, World!"<<endl;
+
+    cout<<"Enter a symbol: ";
+    cin>>symbol;
+
+    cout<<"You entered "<<symbol<<" symbol";
+
+
+    return 0;
+}
